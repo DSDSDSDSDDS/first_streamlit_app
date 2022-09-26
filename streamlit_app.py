c37180e7db74697130d3b410244e522833687781
DSDSDSDSDDS <114049147+DSDSDSDSDDS@users.noreply.github.com>
2022-09-26 11:31:26 +0900

    Update streamlit_app.py
    Formatting updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,17 +29,19 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display Fruityvice API Response
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a Fruit to get information.")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+ 
+ except URLError as e:
+    streamlit.error()
+    
 streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# Normalises returned json data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Writes normalised data to a display grid
-streamlit.dataframe(fruityvice_normalized)
 
 # Trouble shooting hold point
 
