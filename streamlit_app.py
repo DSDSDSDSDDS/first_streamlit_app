b4df015886d8715c87a25b5e8b414eaa3c9255cb
DSDSDSDSDDS <114049147+DSDSDSDSDDS@users.noreply.github.com>
2022-09-26 12:08:14 +0900

    Update streamlit_app.py
    Update load button name

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ streamlit.write('The user entered ', fruit_choice)
 # Trouble shooting hold point
 
 #import snowflake.connector
-streamlit.header("The fruit load list contains:")
+streamlit.header("View or Fruit Load List - Add Your Favourites!")
 # Snowflake related functions
 def get_fruit_load_list():
       with my_cnx.cursor() as my_cur:
@@ -59,9 +59,10 @@ def get_fruit_load_list():
             return my_cur.fetchall()
       
 # Add a button to load the fruit
-if streamlit.button('Get Fruit Load list'):
+if streamlit.button('Get Fruit list'):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       my_data_rows = get_fruit_load_list()
+      my_cnx.close()
       streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
@@ -74,5 +75,6 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the list'):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       back_from_function = insert_row_snowflake(add_my_fruit)
+      my_cnx.close()
       streamlit.text(back_from_function)
 
